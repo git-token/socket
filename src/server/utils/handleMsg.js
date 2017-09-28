a05db0820f404bf0da70b08ae5c0f8f59009790c
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-28 13:44:09 -0700

    WIP

--- src/server/utils/handleMsg.js
+++ src/server/utils/handleMsg.js
@@ -5,52 +5,52 @@ export default function handleMsg({ socket, message }) {
   const { type, data } = JSON.parse(message)
   switch(type.toUpperCase()) {
     case 'WATCH_TOKEN':
-    const { organization } = data
-    const orgData = this.store.getState()['organizations'][organization]
+      const { organization } = data
+      const orgData = this.store.getState()['organizations'][organization]
 
-    // Attempt to send cached data before watching the token
-    if(orgData != null) {
-      socket.send(JSON.stringify({
-        type: 'ORGANIZATION_DATA',
-        org: organization,
-        data: orgData
-      }))
-    } else {
-      this.eventListener.write(message)
-    }
-
-    this.eventListener.pipe(split(JSON.parse)).on('data', (msg) => {
-      try {
-        if (
-          organization == msg['data']['organization'] &&
-          socket.readyState === WebSocket.OPEN
-        ) {
-          socket.send(JSON.stringify({
-            type: 'WATCH_TOKEN',
-            event: msg['event'],
-            org: msg['data']['organization'],
-            id: msg['data']['transactionHash'],
-            data: msg
-          }))
-        }
-      } catch(error) {
-        console.error(error)
+      // Attempt to send cached data before watching the token
+      if(orgData != null) {
+        socket.send(JSON.stringify({
+          type: 'ORGANIZATION_DATA',
+          org: organization,
+          data: orgData
+        }))
+      } else {
+        this.eventListener.write(message)
       }
-    })
 
-    break;
+      this.eventListener.pipe(split(JSON.parse)).on('data', (msg) => {
+        try {
+          if (
+            organization == msg['data']['organization'] &&
+            socket.readyState === WebSocket.OPEN
+          ) {
+            socket.send(JSON.stringify({
+              type: 'WATCH_TOKEN',
+              event: msg['event'],
+              org: msg['data']['organization'],
+              id: msg['data']['transactionHash'],
+              data: msg
+            }))
+          }
+        } catch(error) {
+          console.error(error)
+        }
+      })
+
+      break;
     case 'GET_REGISTERED':
-    this.proxyQuery({
-      queryString: this.queryString[type],
-      type,
-      socket
-    })
-    break;
+      this.proxyQuery({
+        queryString: this.queryString[type],
+        type,
+        socket
+      })
+      break;
     default:
-    socket.send(JSON.stringify({
-      type: 'error',
-      result: `${event} is an invalid event.`
-    }))
-    return null
+      socket.send(JSON.stringify({
+        type: 'error',
+        result: `${event} is an invalid event.`
+      }))
+      return null
   }
 }
